636850196864131e4d7cb855980cb911eb5c748e
Natalie Vautrinot <32278351+TrialbyCompbat@users.noreply.github.com>
2018-05-02 12:46:40 -0400

    Solved Color Issue

--- src/utils/log.js
+++ src/utils/log.js
@@ -11,6 +11,6 @@ const nbaRed = msg => chalk`{bold.hex('#f00b47') ${msg}}`;
 const neonGreen = msg => chalk`{hex('#66ff66') ${msg}}`;
 
 const colorTeamName = (color, name) =>
-  chalk`{bold.white.bgHex('${color}') ${name}}`;
+  chalk`{bold.gray.bgHex('${color}') ${name}}`;
 
 export { error, bold, nbaRed, neonGreen, colorTeamName };
